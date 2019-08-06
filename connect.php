c00e58ef79d5a5e099a8d09d8b6c8a9bf3b3fc7b
ZukisaNante <zukisanante@gmail.com>
2019-08-06 22:21:20 +0200

    verify server connection successful

--- connect.php
+++ connect.php
@@ -1,3 +1,6 @@
 <?php
+//connect to the database server
   $connection = mysql_connect("localhost", "root", "") or die("Couldn't connect to the server");
+  //connect to the database
+  $db = mysql_connect_db("users", $connection) or die ("Couldn't select database");
 ?>
