b6000e9eca1e90d067fd0efe352adadb04a59e53
junior <zukisanante@gmail.com>
2019-08-08 11:34:21 +0200

    notesDatabase

---
+++ notesDatabase/notes.sql
@@ -0,0 +1,8 @@
+/*SQL Database*/
+CREATE DATABASE IF NOT EXISTS Notes_Database;
+CREATE TABLE Notes_Table (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Notes VARCHAR(max) NOT NULL,
+reg_date TIMESTAMP
+);
