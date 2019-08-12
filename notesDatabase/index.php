f74b71d0308769f219683830131d379d847ee8b2
junior <zukisanante@gmail.com>
2019-08-12 15:45:44 +0200

    index.php added

---
+++ notesDatabase/index.php
@@ -0,0 +1,26 @@
+<?php
+    require_once ('config.php');
+    class notes {
+        function select(){
+            $db = new connect;
+            $users = array();
+            $data = $db->prepare('SELECT * FROM MyGuests ORDER BY id');
+            $data->execute();
+            while(
+                $OutputData = $data->fetch(PDO::FETCH_ASSOC)
+            ){
+                $users[$OutputData['id']] = array(
+                    'id' => $OutputData['id'],
+                    'title' => $OutputData['title'],
+                    'notes' => $OutputData['notes'],
+                    'author' => $OutputData['author']
+                );
+            }
+            return json_encode($users);
+        }
+    }
+    $notes = new notes;
+    header('Content_Type: application/json');
+    echo $notes->select();
+
+?>
